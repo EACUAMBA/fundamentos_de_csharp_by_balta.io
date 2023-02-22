b49b79f80b6cb49a5038a4ff9b47888113aca39c
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-02-22 11:43:31 +0200

    Aprendi a utilizar a classe Console
     para mostrar valores no terminal;
     para ler valores introduzidos no terminal;
     para limpar os valores no terminal;
     para impedir a aplicação de fechar após ter executados todas as instruções.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿using System; //Importando o System para termos algumas classes como Console;
 
-namespace Calculator{
-    class Calculator{
-        static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Subtracao();
+        }
+        static void Subtracao(){
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float resultado = v1-v2;
+            Console.WriteLine($"O resultado da subtração entre {v1} e {v2} é {resultado}");
+            Console.ReadKey();// Isto serve para previnir o programa de fechar após executar as instruções.
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma de {v1} e {v2} é {resultado}");// Esta abordagem é chamada de interpolação de Strings, iniciamos uma cadeia de caracteres com o $ (cifrão) e depois podemos utilizar {} (entre chavetas o nome da variável ou uma instrução completa).
+
         }
     }
+
 }
