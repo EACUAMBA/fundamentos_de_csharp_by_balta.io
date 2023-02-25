2877ffaa0f50be5ce3c187b15999d7ffef9749cc
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-02-25 08:24:38 +0200

    Implemented menu

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,79 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Menu();
+
+            Console.ReadKey();// Isto serve para previnir o programa de fechar após executar as instruções.
+        }
+
+        static void Menu(){
+            Console.Clear();
+
+            Console.WriteLine("Cardápio");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine();
+
+            Console.WriteLine("Selecione uma opção:");
+            int itemMenuSelected = int.Parse(Console.ReadLine());
+
+            switch(itemMenuSelected){
+                case(1):{
+                    Soma();
+                    break;
+                }
+                case(2):{
+                    Subtracao();
+                    break;
+                }
+                case(3):{
+                    Multiplicacao();
+                    break;
+                }
+                case(4):{
+                    Divisao();
+                    break;
+                }
+                default: {
+                    Console.WriteLine("O valor introduzido não é válido!");
+                    break;
+                }
+            }
+
+
+        }
+
+        static void Multiplicacao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação entre {v1} x {v2} é {resultado}");
+
+        }
+
+        static void Divisao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão {v1}/{v2} é {resultado}");
         }
         static void Subtracao(){
             Console.Clear();
@@ -21,7 +93,7 @@ namespace Calculator
 
             float resultado = v1-v2;
             Console.WriteLine($"O resultado da subtração entre {v1} e {v2} é {resultado}");
-            Console.ReadKey();// Isto serve para previnir o programa de fechar após executar as instruções.
+            
         }
         static void Soma()
         {
