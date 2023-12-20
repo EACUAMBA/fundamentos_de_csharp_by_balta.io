4b0a210d76a71d4be852494d63b71fee070aa74d
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2023-12-20 23:38:19 +0200

    Implemented the stopwatch structure.

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static void Start()
+{
+    int time = 10;
+
+    int currentTime = 0;
+    while (currentTime != time)
+    {
+        currentTime++;
+    }
+}
+
+Start();
