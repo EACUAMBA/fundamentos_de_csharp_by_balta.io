02e1a13f7181676f55ab822881213b2a197cbae3
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2023-12-21 00:10:46 +0200

    Using ReadLine to get data from user input.

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,10 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-static void Start()
-{
-    int time = 10;
+static void Menu(){
+    Console.Clear();
+    Console.WriteLine("S = Segundo => 10s -> 10 segundos");
+    Console.WriteLine("M = Minuto => 10m-> 10 minutos");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
 
+    string data = Console.ReadLine().ToLower();
+    Console.WriteLine(data);
+}
+
+Menu();
+
+static void Start(int time)
+{
     int currentTime = 0;
     while (currentTime != time)
     {
@@ -14,6 +25,10 @@ static void Start()
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
     }
+
+    Console.Clear();
+    Console.WriteLine("StopWatch ended!");
+    Thread.Sleep(25000);
 }
 
-Start();
+//Start(6);
